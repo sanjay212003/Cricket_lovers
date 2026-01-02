174824ba608eae932c0e28861d70b9d4048e48e4
sanjay212003 <sanjayumeshbabu@gmail.com>
2026-01-02 08:32:14 +0530

    server url chnaged in auth serivce

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -3,10 +3,13 @@ import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const loginUser = async (phone, password) => {
-  const response = await axios.post(`${API_URL}/login`, {
-    phone,
-    password
-  });
+  const response = await axios.post(
+    `${API_URL}/api/auth/login`,
+    {
+      phone,
+      password
+    }
+  );
 
   return response.data;
 };
