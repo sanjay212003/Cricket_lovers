d1cbc3ec23857a46caaed15bb9327cf41c712874
sanjay212003 <sanjayumeshbabu@gmail.com>
2026-01-02 08:09:16 +0530

    Fix: use Render assigned PORT

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
 import placementRoutes from "./routes/placementRoutes.js";
 
+const PORT = process.env.PORT || 5000;
+
 dotenv.config();
 
 const app = express();
@@ -28,7 +30,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ MongoDB connected");
-    app.listen(process.env.PORT || 5000, () => {
+    app.listen(PORT, () => {
       console.log(
         `🚀 Server running on port ${process.env.PORT || 5000}`
       );
